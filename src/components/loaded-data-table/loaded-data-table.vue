f5d2090612068664ea8c147793929256183310a1
orlandobel <orlando.bel.jimmy@gmail.com>
2026-03-31 23:46:43 -0600

    loaded data table get headers and data from props

--- src/components/loaded-data-table/loaded-data-table.vue
+++ src/components/loaded-data-table/loaded-data-table.vue
@@ -1,44 +1,25 @@
 <script setup lang="ts">
+defineProps<{
+    headers: string[]
+    data: any[]
+}>()
 </script>
 
 <template>
     <table class="table-fixed w-full border border-gray-400 border-collapse">
         <thead>
             <tr>
-                <th class="border border-gray-400 py-2">Header 1</th>
-                <th class="border border-gray-400 py-2">Header 2</th>
-                <th class="border border-gray-400 py-2">Header 3</th>
-                <th class="border border-gray-400 py-2">Header 4</th>
-                <th class="border border-gray-400 py-2">Header 5</th>
-                <th class="border border-gray-400 py-2">Header 6</th>
+                <th class="border border-gray-400 py-2" v-for="header in headers" :key="header">
+                    {{ header }}
+                </th>
             </tr>
         </thead>
 
         <tbody>
-            <tr>
-                <td class="border border-gray-400 px-2 py-1">Data 0-1</td>
-                <td class="border border-gray-400 px-2 py-1">Data 0-2</td>
-                <td class="border border-gray-400 px-2 py-1">Data 0-3</td>
-                <td class="border border-gray-400 px-2 py-1">Data 0-4</td>
-                <td class="border border-gray-400 px-2 py-1">Data 0-5</td>
-                <td class="border border-gray-400 px-2 py-1">Data 0-6</td>
-            </tr>
-            <tr>
-                <td class="border border-gray-400 px-2 py-1">Data 1-1</td>
-                <td class="border border-gray-400 px-2 py-1">Data 1-2</td>
-                <td class="border border-gray-400 px-2 py-1">Data 1-3</td>
-                <td class="border border-gray-400 px-2 py-1">Data 1-4</td>
-                <td class="border border-gray-400 px-2 py-1">Data 1-5</td>
-                <td class="border border-gray-400 px-2 py-1">Data 1-6</td>
-            </tr>
-    
-            <tr>
-                <td class="border border-gray-400 px-2 py-1">Data 2-1</td>
-                <td class="border border-gray-400 px-2 py-1">Data 2-2</td>
-                <td class="border border-gray-400 px-2 py-1">Data 2-3</td>
-                <td class="border border-gray-400 px-2 py-1">Data 2-4</td>
-                <td class="border border-gray-400 px-2 py-1">Data 2-5</td>
-                <td class="border border-gray-400 px-2 py-1">Data 2-6</td>
+            <tr v-for="row in data" :key="row">
+                <td class="border border-gray-400 px-2 py-1" v-for="value in row" :key="value">
+                    {{ value }}
+                </td>
             </tr>
         </tbody>
     </table>
--- src/pages/belt-grade-promossion-exam/belt-grade-promossion-exam.vue
+++ src/pages/belt-grade-promossion-exam/belt-grade-promossion-exam.vue
@@ -5,6 +5,38 @@ import LoadedDataTable from '@/components/loaded-data-table/loaded-data-table.vu
 type TabKey = 'datos' | 'previsualizacion'
 const activeTab = ref<TabKey>('datos')
 
+const headers = [
+	'Escuela',
+	'Profesor',
+	'Alumno',
+	'Grado',
+	'Talla',
+]
+
+const data = [
+	{
+		'Escuela': 'Escuela 1',
+		'Profesor': 'Profesor 1',
+		'Alumno': 'Alumno 1',
+		'Grado': 'Grado 1',
+		'Talla': 'Talla 1'
+	},
+	{
+		'Escuela': 'Escuela 2',
+		'Profesor': 'Profesor 2',
+		'Alumno': 'Alumno 2',
+		'Grado': 'Grado 2',
+		'Talla': 'Talla 2'
+	},
+	{
+		'Escuela': 'Escuela 3',
+		'Profesor': 'Profesor 3',
+		'Alumno': 'Alumno 3',
+		'Grado': 'Grado 3',
+		'Talla': 'Talla 3'
+	}
+]
+
 </script>
 
 <template>
@@ -17,9 +49,9 @@ const activeTab = ref<TabKey>('datos')
 
 			<v-divider />
 
-			<v-tabs-window class="p-1" v-model="activeTab">
-				<v-tabs-window-item value="datos">
-					<loaded-data-table />
+			<v-tabs-window v-model="activeTab">
+				<v-tabs-window-item class="px-1" value="datos">
+					<loaded-data-table :headers="headers" :data="data" />
 				</v-tabs-window-item>
 
 				<v-tabs-window-item value="previsualizacion">
