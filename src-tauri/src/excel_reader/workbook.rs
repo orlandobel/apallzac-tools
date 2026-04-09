7073f21fa104e40dfe11807f7fba030faefe2bc5
orlando.bel.jimmy@gmail.com <orlando.belmonte@mienvio.mx>
2026-04-09 14:42:33 -0600

    Remove unused function from Workbook

--- src-tauri/src/excel_reader/workbook.rs
+++ src-tauri/src/excel_reader/workbook.rs
@@ -1,9 +1,10 @@
-use super::column_configurations::{BeltPromotionConfiguration, Configurations};
 use excelstream::{
     streaming_reader::{RowStructIterator, StreamingReader},
     ExcelReader,
 };
 
+// TODO :: function to reload file
+#[allow(dead_code)]
 pub struct Workbook {
     path: String,
     reader: StreamingReader,
@@ -19,16 +20,6 @@ impl Workbook {
     pub fn get_sheet(&mut self) -> Result<RowStructIterator<'_>, Box<dyn std::error::Error>> {
         let sheet = self.reader.rows_by_index(0)?;
         Ok(sheet)
-    }
-
-    fn get_column_config(
-        &mut self,
-        first_row: &excelstream::Row,
-    ) -> Result<Configurations, Box<dyn std::error::Error>> {
-        let config =
-            Configurations::BeltPromotionConfiguration(BeltPromotionConfiguration::new(first_row)?);
-
-        Ok(config)
     }
 }
 
@@ -48,26 +39,6 @@ mod tests {
         assert_eq!(workbook.path, path_str);
 
         println!("✓ Test exitoso: Workbook creado y Excel leído correctamente");
-    }
-
-    #[test]
-    fn test_get_column_config() {
-        let path_str = get_file_path("examen ejemplo 1.xlsx");
-
-        // Crear el workbook y leer el Excel
-        let mut workbook = Workbook::new(path_str).expect("No se pudo crear Workbook");
-
-        let mut sheet = workbook.get_sheet().expect("No se pudo obtener la hoja");
-        let row = sheet.next().unwrap().expect("No se pudo obtener la fila");
-        drop(sheet); // Liberar la referencia mutable a workbook
-
-        let column_config = workbook.get_column_config(&row);
-        assert!(
-            column_config.is_ok(),
-            "No se pudo obtener la configuración de columnas"
-        );
-
-        println!("✓ Test exitoso: Configuración de columnas obtenida correctamente");
     }
 
     /* This function is intended to get the file path for the test Excel file, is not a test it self */
