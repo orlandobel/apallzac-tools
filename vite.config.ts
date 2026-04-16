0e2e3a09beb798a801ee463c6a50cbf89ce7860e
orlando.bel.jimmy@gmail.com <orlando.belmonte@mienvio.mx>
2026-04-16 10:52:31 -0600

    Try macod fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
+  base: "./",
   resolve: {
     alias: {
       // Alias compatible con Bun y Vite
